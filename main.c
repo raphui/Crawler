161cfe31b9b1fc7a3f1f33d63a772bf289d26708
raphui <raphio134@hotmail.fr>
2013-08-13 13:24:12 -0700

    v2.0 beta

--- main.c
+++ main.c
@@ -5,8 +5,113 @@
 #include <tidy/tidy.h>
 #include <tidy/buffio.h>
 
-FILE *reader = NULL;
-FILE *urlFile = NULL;
+#define HISTORY_SIZE	1024
+
+typedef struct node
+{
+	unsigned char passed;
+	char url[255];
+	struct node *childs;
+	struct node *siblings;
+
+}node;
+
+
+node *tree;
+node *current;
+
+int current_depth;
+
+typedef struct record
+{
+	node *n;
+	int depth;
+}record;
+
+record history[HISTORY_SIZE];
+
+struct node *createTree( void )
+{
+	node *root = ( node * )malloc( sizeof( node ) );
+	root->passed = 1;
+	root->childs = NULL;
+	root->siblings = NULL;
+
+	return root;
+}
+
+struct node *addSiblings( node **n , node *item )
+{
+	(*n)->siblings = ( node * )malloc( sizeof( node ) );
+	memcpy( (*n)->siblings , item , sizeof( node ) );
+
+	return (*n)->siblings;
+}
+
+struct node *addChilds( node **n , node *item )
+{
+	node *tmp;
+
+	if( (*n)->childs != NULL )
+	{	
+		tmp = (*n)->childs;
+		
+		if( tmp->siblings != NULL )	
+			while( tmp->siblings != NULL )
+				tmp = tmp->siblings;
+		
+	
+		tmp->siblings = ( node * )malloc( sizeof( node ) );
+		memcpy( tmp->siblings , item , sizeof( node ) );
+		tmp->siblings->childs = NULL;
+		tmp->siblings->siblings = NULL;
+	}
+	else
+	{
+		(*n)->childs = ( node * )malloc( sizeof( node ) );
+		memcpy( (*n)->childs , item , sizeof( node ) );
+		tmp = (*n)->childs;
+		tmp->childs = NULL;
+		tmp->siblings = NULL;
+	}
+
+	return tmp;
+}
+
+void addNodeInHistory( node *t )
+{
+	static int i = 0;
+	record *tmp;
+
+	tmp = &history[i];
+	tmp->n = t;
+	tmp->depth = current_depth;
+	i++;
+
+}
+
+int isNodeInHistory( node *t )
+{
+	int j = 0;
+	int ret = 0;
+	record *tmp;
+
+	for( j == 0 ; j < HISTORY_SIZE ; j++ )
+	{
+		tmp = &history[j];
+		
+		if( tmp->n == NULL )
+			continue;
+	
+		if( !strcmp( t->url , tmp->n->url ) && ( current_depth >= tmp->depth ) )
+		{
+			return 1; 
+		}
+	}
+
+	return 0;
+}
+
 
 CURL *handle = NULL;
 
@@ -25,26 +130,16 @@ uint write_cb(char *in, uint size, uint nmemb, TidyBuffer *out)
     return r;
 }
 
-void openFile( void )
-{
-    urlFile = fopen("/tmp/urlFile.txt" , "w+");
-    reader = fopen("/tmp/urlFile.txt" , "r");
 
-    if( urlFile == NULL || reader == NULL )
-    {
-        printf("Cannot open file parse().\n");
-
-        exit( 1 );
-    }
-}
-
-void parse( TidyDoc doc , TidyNode node )
+void parse( TidyDoc doc , TidyNode n )
 {
     TidyNode child;
 
+	node *item;
+
     char str[] = "a";
 
-    for( child = tidyGetChild( node ) ; child ; child = tidyGetNext( child ) )
+    for( child = tidyGetChild( n ) ; child ; child = tidyGetNext( child ) )
     {
         ctmbstr name = tidyNodeGetName( child );
 
@@ -63,9 +158,12 @@ void parse( TidyDoc doc , TidyNode node )
                         {
                             if( strstr( tidyAttrValue( attr ) , "http://" ) != NULL )
                             {
-                                fprintf( urlFile , "%s\n" , tidyAttrValue( attr ) );
+								item = ( node * )malloc( sizeof( node ) );
+								item->passed = 0;
+								sprintf( item->url , "%s" , tidyAttrValue( attr ) );
 
-                                //printf("%s\n" , tidyAttrValue( attr ) );
+								addChilds( &current , item );
+								free( item );
                             }
                         }
 
@@ -79,16 +177,20 @@ void parse( TidyDoc doc , TidyNode node )
     }
 }
 
-void crawl( const char *url )
+void crawl( node *n )
 {
     CURLcode ret;
     static int i = 0;
+	//static int depth = 0;
     int err;
-    char *goturl = ( char * )malloc( 255 * sizeof( char ) );
+	node *tmp;
 
-    memset( goturl , 0 , 255 );
+	current = n;
+	addNodeInHistory( n );
 
-    curl_easy_setopt( handle , CURLOPT_URL , url );
+	printf("Link number: %d     URL got : %s\n" , i++ , n->url );
+
+    curl_easy_setopt( handle , CURLOPT_URL , n->url );
     curl_easy_setopt( handle , CURLOPT_WRITEFUNCTION , write_cb );
     curl_easy_setopt( handle , CURLOPT_WRITEDATA , &docbuff );
 
@@ -114,28 +216,73 @@ void crawl( const char *url )
 
                 if( err >= 0 )
                 {
-                    if( urlFile == NULL && reader == NULL )
-                        openFile();
 
-                    parse( tdoc , tidyGetRoot( tdoc ) );
+					while( 1 )//( n->childs != NULL ) || ( n->siblings != NULL ) )
+					{
 
-                    while( fgets( goturl , 255 , reader ) != NULL )
-                    {
-                        printf("Link number: %d     URL got : %s" , i++ , goturl );
+                    	parse( tdoc , tidyGetRoot( tdoc ) );
 
-                        crawl( goturl );
-                    }
+						if( n->childs == NULL )
+						{
+							
+							tmp = n->siblings;
 
-                    fclose( urlFile );
-                    fclose( reader );
-                }
+							if( ( tmp->siblings == NULL ) || ( tmp == NULL ) )
+								exit( 1 );
+							
+							
+							while( isNodeInHistory( tmp ) == 1 )
+							{
+								if( tmp->siblings == NULL )
+									break;
+								else
+									tmp = tmp->siblings;
+							}
+	
+							crawl( tmp );
+						}
+						else
+						{
+							if( isNodeInHistory( n->childs ) == 1 )
+							{
+
+								tmp = n->childs->siblings;
+								
+								if( ( tmp->siblings == NULL ) || ( tmp == NULL ) )
+									exit( 1 );
+
+								while( isNodeInHistory( tmp ) == 1 )
+								{
+									if( tmp->siblings == NULL )
+										break;
+									else
+										tmp = tmp->siblings;
+								}
+							}
+							else
+							{
+								tmp = n->childs;
+							}
+							
+							current_depth++;
+							crawl( tmp );//n->childs );
+							current_depth--;
+						}
+					}
+
+				}
             }
         }
     }
 }
+	
 
 int main( void )
 {
+	tree = NULL;
+	tree = createTree();
+	current = tree;		
+
     tdoc = tidyCreate();
 
     tidyOptSetBool( tdoc , TidyForceOutput , yes );
@@ -146,8 +293,12 @@ int main( void )
     curl_global_init( CURL_GLOBAL_ALL );
 
     handle = curl_easy_init();
+	
+	sprintf( tree->url , "http://www.lefigaro.fr" );
 
-    crawl("http://www.racerxonline.com/");
+    //crawl("http://www.racerxonline.com/");
+	//crawl("http://www.lefigaro.fr");
+	crawl( tree );
 
     curl_easy_cleanup( handle );
 
